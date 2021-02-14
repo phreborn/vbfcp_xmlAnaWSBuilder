1f44c870a18f08e4deff5d09572b5dc48487be28
Hongtao Yang <Hongtao.Yang@cern.ch>
2021-02-13 16:38:32 -0800

    Minor fix to installation procedure

--- scripts/install_roofitext.sh
+++ scripts/install_roofitext.sh
@@ -10,12 +10,22 @@ fi
 if [ ! -d ${outputDir}/RooFitExtensions ]; then
     mkdir -vp ${outputDir}
     echo "Cloning RooFitExtensions into target directory ${outputDir}..."
-    git clone https://gitlab.cern.ch/atlas_higgs_combination/software/RooFitExtensions.git ${outputDir}/RooFitExtensions
-    if [ -d ${outputDir}/RooFitExtensions ]; then
-        pushd ${outputDir}/RooFitExtensions
-        cmake . && make -j 4
-        popd
-    fi
-
-    export RooFitExtensions_DIR=${outputDir}/RooFitExtensions
+    git clone ssh://git@gitlab.cern.ch:7999/atlas_higgs_combination/software/RooFitExtensions.git ${outputDir}/RooFitExtensions
 fi
+
+if [ -d ${outputDir}/RooFitExtensions ]; then
+    pushd ${outputDir}/RooFitExtensions
+    git pull
+    mkdir -vp build
+    cd build
+    cmake ../ -DCMAKE_INSTALL_PREFIX=${outputDir}/RooFitExtensions
+    make -j 4
+    make install
+    cd -
+    popd
+fi
+
+export RooFitExtensions_DIR=${outputDir}/RooFitExtensions
+
+mkdir -vp ${outputDir}/lib/
+cp ${outputDir}/RooFitExtensions/lib/* ${outputDir}/lib/
