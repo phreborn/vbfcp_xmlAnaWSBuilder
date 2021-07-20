03d3d3ce3b0c288c915a411410b80f8f999afcbc
chen hui run <huirun@lxslc708.ihep.ac.cn>
2021-07-20 22:47:03 +0800

    vbfcp should pick: autonll

--- run/copydtd.sh
+++ run/copydtd.sh
@@ -1,9 +1,10 @@
-ds=$(ls config)
+config=$1
+ds=$(ls ${config})
 
 for d in $ds;do
 
-  cp AnaWSBuilder.dtd config/${d}/
-  cp AnaWSBuilder.dtd config/${d}/channel/
-  cp AnaWSBuilder.dtd config/${d}/model
+  cp AnaWSBuilder.dtd ${config}/${d}/
+  cp AnaWSBuilder.dtd ${config}/${d}/channel/
+  cp AnaWSBuilder.dtd ${config}/${d}/model
 
 done
---
+++ run/logNLL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat=AllCats
+for suffix in statOnly allSys ssSys jetSys photonSys;do
+  outnll=autonll/dNLL_${suffix}.log
+  > ${outnll}
+  tmpall=tmpall
+  root -b -q getNLL.cxx\(\"out${cat}_${suffix}\"\) | grep nll | cut -d : -f 2 > ${tmpall}
+  cat ${tmpall} | grep m | sort -r | sed 's/m/-0\./g' >> ${outnll}
+  cat ${tmpall} | grep p | sed 's/p/0\./g' >> ${outnll}
+  rm ${tmpall}
+done
