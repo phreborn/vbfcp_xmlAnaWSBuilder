bc7c4feeca476e86df89af7d3717f73d105f5f9e
chen hui run <huirun@lxslc708.ihep.ac.cn>
2021-07-20 22:47:03 +0800

    vbfcp should pick: autonll

--- run/getNLL.cxx
+++ run/getNLL.cxx
@@ -12,17 +12,46 @@ void getNLL(TString fitresult = "outAllCats_statOnly"){
 //void getNLL(TString fitresult = "outLL_statOnly"){
 //void getNLL(TString fitresult = "outLL_allSys"){
   map<TString, TString> cHWs;
-  cHWs["m2d5"] = "-2.5";
-  cHWs["m2d0"] = "-2";
-  cHWs["m1d5"] = "-1.5";
-  cHWs["m1d0"] = "-1";
-  cHWs["m0d5"] = "-0.5";
-  cHWs["m0d0"] = "-0";
-  cHWs["p0d5"] = "0.5";
-  cHWs["p1d0"] = "1";
-  cHWs["p1d5"] = "1.5";
-  cHWs["p2d0"] = "2";
-  cHWs["p2d5"] = "2.5";
+  cHWs["m0d05"] = "-0.05";
+  cHWs["p0d05"] = "0.05";
+  cHWs["m0d15"] = "-0.15";
+  cHWs["p0d15"] = "0.15";
+  cHWs["m0d10"] = "-0.1";
+  cHWs["p0d10"] = "0.1";
+  cHWs["m0d25"] = "-0.25";
+  cHWs["p0d25"] = "0.25";
+  cHWs["m0d20"] = "-0.2";
+  cHWs["p0d20"] = "0.2";
+  cHWs["m0d35"] = "-0.35";
+  cHWs["p0d35"] = "0.35";
+  cHWs["m0d30"] = "-0.3";
+  cHWs["p0d30"] = "0.3";
+  cHWs["m0d45"] = "-0.45";
+  cHWs["p0d45"] = "0.45";
+  cHWs["m0d40"] = "-0.4";
+  cHWs["p0d40"] = "0.4";
+  cHWs["m0d55"] = "-0.55";
+  cHWs["p0d55"] = "0.55";
+  cHWs["m0d50"] = "-0.5";
+  cHWs["p0d50"] = "0.5";
+  cHWs["m0d65"] = "-0.65";
+  cHWs["p0d65"] = "0.65";
+  cHWs["m0d60"] = "-0.6";
+  cHWs["p0d60"] = "0.6";
+  cHWs["m0d75"] = "-0.75";
+  cHWs["p0d75"] = "0.75";
+  cHWs["m0d70"] = "-0.7";
+  cHWs["p0d70"] = "0.7";
+  cHWs["m0d85"] = "-0.85";
+  cHWs["p0d85"] = "0.85";
+  cHWs["m0d80"] = "-0.8";
+  cHWs["p0d80"] = "0.8";
+  cHWs["m0d95"] = "-0.95";
+  cHWs["p0d95"] = "0.95";
+  cHWs["m0d90"] = "-0.9";
+  cHWs["p0d90"] = "0.9";
+  cHWs["m0d00"] = "-1.56e-07";
+  cHWs["p1d00"] = "1";
   map<TString, double> d_map;
   d_map["m00"] = 0.;
   d_map["m01"] = -0.01;
@@ -62,6 +91,7 @@ void getNLL(TString fitresult = "outAllCats_statOnly"){
   t_SM->SetBranchAddress("nll", &nll_SM);
   t_SM->GetEntry(0);
 
+  double minNLL = nll_SM;
   for(auto d = d_map.begin(); d != d_map.end(); d++){
   //for(auto d = cHWs.begin(); d != cHWs.end(); d++){
     TFile *f = new TFile(fitresult+"/out_"+d->first+".root", "read");
@@ -71,7 +101,20 @@ void getNLL(TString fitresult = "outAllCats_statOnly"){
     t->SetBranchAddress("nll", &nll);
     t->GetEntry(0);
 
-    cout<<d->first<<" "<<nll - nll_SM<<endl;
+    if(nll < minNLL) minNLL = nll;
+    delete f;
+  }
+
+  for(auto d = d_map.begin(); d != d_map.end(); d++){
+  //for(auto d = cHWs.begin(); d != cHWs.end(); d++){
+    TFile *f = new TFile(fitresult+"/out_"+d->first+".root", "read");
+    TTree *t = (TTree*)f->Get("nllscan");
+
+    double nll;
+    t->SetBranchAddress("nll", &nll);
+    t->GetEntry(0);
+
+    cout<<"nll:"<<d->first<<" "<<nll - minNLL<<endl;
     delete f;
   }
 
