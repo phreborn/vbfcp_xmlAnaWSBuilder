538cce8b47c18c5338eb4bfdacda7fa5eba789a2
chen hui run <huirun@lxslc708.ihep.ac.cn>
2021-10-18 21:41:50 +0800

    mod genSubmit.sh logNLL.sh sub_topXml.sh

--- run/logNLL.sh
+++ run/logNLL.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cat=AllCats_SMEFT
-for suffix in statOnly allSys ssSys jetSys photonSys;do
-  outnll=autonll/dNLL_${suffix}.log
+#cat=LL_SMEFT
+for suffix in statOnly allSys ssSys jetSys photonSys theorySys;do
+#for suffix in statOnly allSys;do
+  outnll=autonll/dNLL_${cat}_${suffix}.log
   > ${outnll}
   tmpall=tmpall
   root -b -q getNLL.cxx\(\"out${cat}_${suffix}\"\) | grep nll | cut -d : -f 2 | sed 's/d/\./g' > ${tmpall}
--- run/sub_topXml.sh
+++ run/sub_topXml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for d in `ls ${1}/vbf_cp_* -d`;do
-  dtilde=`echo ${d} | cut -d "-" -f 2 | cut -d "_" -f 3`
+  dtilde=`echo ${d} | cut -d "/" -f 2 | cut -d "_" -f 3`
+  echo ${dtilde}
   cp /scratchfs/atlas/huirun/atlaswork/VBF_CP/syst/config/vbf_cp_${dtilde}/d_tilde_${dtilde}.xml ${1}/vbf_cp_${dtilde}/d_tilde_${dtilde}.xml
 done
