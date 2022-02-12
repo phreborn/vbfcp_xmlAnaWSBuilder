a1fe1a2855b128f292ac59c994e627f8f32674dd
chen hui run <huirun@lxslc704.ihep.ac.cn>
2022-02-12 14:14:51 +0800

    minor

--- run/sub_topXml.sh
+++ run/sub_topXml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for d in `ls ${1}/vbf_cp_* -d`;do
-  dtilde=`echo ${d} | cut -d "-" -f 2 | cut -d "_" -f 3`
+  dtilde=`echo ${d} | cut -d "/" -f 2 | cut -d "_" -f 3`
   cp /scratchfs/atlas/huirun/atlaswork/VBF_CP/syst/config/vbf_cp_${dtilde}/d_tilde_${dtilde}.xml ${1}/vbf_cp_${dtilde}/d_tilde_${dtilde}.xml
 done
